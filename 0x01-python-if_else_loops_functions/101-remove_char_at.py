4b44870ad6e0e0f8c8809cc9e5527228b85af344
LoveCode20 <amysweat20@gmail.com>
2023-10-25 01:09:25 -0700

    new

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    i = 0
+    new_str = ""
+    for ch in str:
+        if i != n:
+            new_str += ch
+        i += 1
+    har_at(str, n):
+            i = 0
+                new_str = ""
+                    for ch in str:
+                                if i != n:
+                                                new_str += ch
+                                                        i += 1
+                                                            return
