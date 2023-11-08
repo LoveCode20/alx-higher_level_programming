8080d58cfbf4e3b0f5f264e7e5942fd2f89b0929
LoveCode20 <amysweat20@gmail.com>
2023-11-07 19:39:09 -0800

    new

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" read_file module """
+
+
+def read_file(filename=""):
+    """
+    read_file function
+    reads a test file(UTF8) and prints its result to stdout
+    """
+    with open(filename, mode="r", encoding="")
