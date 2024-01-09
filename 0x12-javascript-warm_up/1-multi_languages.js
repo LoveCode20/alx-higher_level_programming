6eb4322d8ab8dc766b2b070bf2bae9729945144c
LoveCode20 <amysweat20@gmail.com>
2024-01-09 09:28:24 -0800

    updates

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-const cVar = "C is fun";
-const piVar = "Python is cool";
-const myVar = "JavaScript is amazing";
-console.log(cVar);
-console.log(piVar);
-console.log(myVar);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
